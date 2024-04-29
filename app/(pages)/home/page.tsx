b375c5e543d8b7fc5fd42129d927501c4ee53c03
CordobaBenjamin <cordobabenjamin37@gmail.com>
2024-04-29 04:36:34 -0300

    new sections and css for home (slider and categorys)

--- app/(pages)/home/page.tsx
+++ app/(pages)/home/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import Image from "next/image";
+import Image from "next/legacy/image";
 import {
   FirstImage,
   SecondImage,
@@ -17,48 +17,46 @@ import {
 import "animate.css";
 
 const IndexPage = () => {
-
   const [sliderComponent, setSliderComponent] = useState(ThirdSlider);
   const [stepSliderComponent, setStepSliderComponent] = useState(3);
   console.log(stepSliderComponent);
-  
+
   const toLeftButton = () => {
     console.log("toLeftButton Fn");
-    let newStepSliderComponent = stepSliderComponent - 1;  
+    let newStepSliderComponent = stepSliderComponent - 1;
     setStepSliderComponent(newStepSliderComponent);
-      switch (newStepSliderComponent) {
-        case 0:
-          setStepSliderComponent(stepSliderComponent + 5);
-          break;
-        case 1:
-          setSliderComponent(FirstSlider);
-          break;
-        case 2:
-          setSliderComponent(SecondSlider);
-          break;
-        case 3:
-          setSliderComponent(ThirdSlider);
-          break;
-        case 4:
-          setSliderComponent(FourthSlider);
-          break;
-        case 5:
-          setSliderComponent(FiveSlider);
-          break;
-        case 6:
-          setStepSliderComponent(stepSliderComponent - 5);
-          break;
-        default:
-          setSliderComponent(ThirdSlider);
-          break;
-      }
-    };
-  
+    switch (newStepSliderComponent) {
+      case 0:
+        setStepSliderComponent(stepSliderComponent + 5);
+        break;
+      case 1:
+        setSliderComponent(FirstSlider);
+        break;
+      case 2:
+        setSliderComponent(SecondSlider);
+        break;
+      case 3:
+        setSliderComponent(ThirdSlider);
+        break;
+      case 4:
+        setSliderComponent(FourthSlider);
+        break;
+      case 5:
+        setSliderComponent(FiveSlider);
+        break;
+      case 6:
+        setStepSliderComponent(stepSliderComponent - 5);
+        break;
+      default:
+        setSliderComponent(ThirdSlider);
+        break;
+    }
+  };
 
   const toRightButton = () => {
     console.log("toRightButton Fn");
-  let newStepSliderComponent = stepSliderComponent + 1;  
-  setStepSliderComponent(newStepSliderComponent);
+    let newStepSliderComponent = stepSliderComponent + 1;
+    setStepSliderComponent(newStepSliderComponent);
 
     switch (newStepSliderComponent) {
       case 0:
@@ -84,7 +82,6 @@ const IndexPage = () => {
         break;
       default:
         setSliderComponent(ThirdSlider);
-        
     }
   };
 
@@ -117,43 +114,84 @@ const IndexPage = () => {
 
   return (
     <main className={`bg-neutral-800`}>
-      <div id="image" className={`p-4`}>
+      <div className={`flex flex-col pt-5 mx-5`}>
+        <div className="block">
+          <span className={`py-2 text-5xl text-neutral-300`}> CORDOBA </span>
+        </div>
+        <div className="flex justify-end">
+          <span className={`py-2 font-serif text-5xl text-neutral-300 `}>
+            {" "}
+            WOODS{" "}
+          </span>
+        </div>
+      </div>
+
+      <div id="image" className={`my-4 mb-8 border-white border-y-8`}>
         {renderCurrentComponent()}
       </div>
-      <div id="buttons" className={`flex flex-row justify-between mt-2`}>
+      <div id="buttons" className={`flex flex-row justify-center `}>
         <div
           id="ver mas"
-          className={`border-y-2 border-neutral-600  w-fit mx-10 p-2 rounded-lg`}
+          className={`border-y-2 p-2 rounded- border-neutral-400  w-fit mb-4`}
         >
           {" "}
-          <button className={``}>Ver Mas</button>{" "}
-        </div>
-        <div
-          id="añadir al carrito"
-          className={`border-y-2 border-neutral-600 w-fit mx-10 p-2 rounded-lg`}
-        >
-          <button>Añadir al carrito</button>
+          <button className={`text-xl`}>Productos de categoria !</button>{" "}
         </div>
       </div>
       <div
         id="product"
-        className={`flex flex-col mt-5 border-t-2 border-t-neutral-700`}
+        className={`flex flex-col mt-5 border-y-4 border-t-neutral-500`}
       >
-        <div className={`flex justify-between my-10`}>
-          <div
-            id="left"
-            className={`border-2 border-neutral-600 h-fit ml-5 my-auto`}
-          >
-            <button type="button" onClick={toLeftButton}>left</button>
+        <div className={`flex flex-col pt-5 mx-auto`}>
+          <div className="">
+            <span className={`font-medium text-2xl text-neutral-300`}>
+              {" "}
+              NUESTRA SECCION DE SILLAS{" "}
+            </span>
+          </div>
+        </div>
+
+        <div className={`flex justify-between`}>
+          <div id="left" className={` h-fit ml-5 my-auto`}>
+            <button type="button" onClick={toLeftButton}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m18.75 4.5-7.5 7.5 7.5 7.5m-6-15L5.25 12l7.5 7.5"
+                />
+              </svg>
+            </button>
           </div>
 
-          {sliderComponent}
+          <div id="image" className={`p-4 rounded-xl`}>
+            {sliderComponent}
+          </div>
 
-          <div
-            id="right"
-            className={`border-2 border-neutral-600 h-fit mr-5 my-auto`}
-          >
-            <button type="button" onClick={toRightButton}>right</button>
+          <div id="right" className={`h-fit mr-5 my-auto`}>
+            <button type="button" onClick={toRightButton}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5"
+                />
+              </svg>
+            </button>
           </div>
         </div>
         <div
@@ -162,6 +200,73 @@ const IndexPage = () => {
         >
           {" "}
           <button> Todos nuestros productos </button>{" "}
+        </div>
+      </div>
+      <div>
+        <h4 className={`mx-auto w-fit my-4 text-2xl font-semibold`}>
+          {" "}
+          SECCIONES DE INTERES {" "}
+        </h4>
+        <div className={`flex flex-col mx-auto py-4 w-fit`}>
+          <div className={`mb-6`}>
+            <div
+              className={`relative w-56 h-64 shadow-lg shadow-neutral-600  `}
+            >
+              <Image
+                src="/homeSource/BedFurniture.jpg"
+                className={`rounded-lg`}
+                alt="Chair"
+                layout="fill"
+                objectFit="cover"
+              />
+            </div>
+            <div className={`flex flex-row justify-center mt-3 `}>
+              <div className={``}>
+                {" "}
+                <h2 > Decora tu Dormitorio </h2>{" "}
+              </div>
+            </div>
+          </div>
+
+          <div className={`mb-6`}>
+            <div
+              className={`relative w-56 h-64 shadow-lg shadow-neutral-600  `}
+            >
+              <Image
+                src="/homeSource/LivingFurniture.jpg"
+                className={`rounded-lg`}
+                alt="Chair"
+                layout="fill"
+                objectFit="cover"
+              />
+            </div>
+            <div className={`flex flex-row justify-center mt-3 `}>
+              <div className={``}>
+                {" "}
+                <h2 > Administra tu espacio </h2>{" "}
+              </div>
+            </div>
+          </div>
+
+          <div className={`mb-6`}>
+            <div
+              className={`relative w-56 h-64 shadow-lg shadow-neutral-600  `}
+            >
+              <Image
+                src="/homeSource/lightSection.jpg"
+                className={`rounded-lg`}
+                alt="Chair"
+                layout="fill"
+                objectFit="cover"
+              />
+            </div>
+            <div className={`flex flex-row justify-center mt-3 `}>
+              <div className={``}>
+                {" "}
+                <h2 > Ilumina tu Hogar </h2>{" "}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </main>
--- app/src/homeComponents/slider.tsx
+++ app/src/homeComponents/slider.tsx
@@ -1,8 +1,8 @@
-import Image from "next/image";
+import Image from "next/legacy/image";
 
 export const FirstSlider = () => {
   return (
-    <div id="image" className={`border-2 border-neutral-700 p-4 rounded-xl`}>
+    <>
       <div className={`relative w-56 h-56 shadow-lg shadow-neutral-600  `}>
         <Image
           src="/homeSource/chairSection1.jpg"
@@ -15,20 +15,20 @@ export const FirstSlider = () => {
       <div className={`flex flex-row justify-between mt-5 `}>
         <div className={``}>
           {" "}
-          <h2> $10000 </h2>{" "}
+          <h2> $14.000 </h2>{" "}
         </div>
         <div className={``}>
           {" "}
-          <h2> Silla Ergonomica </h2>{" "}
+          <h2> Silla Ergonomica</h2>{" "}
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
 export const SecondSlider = () => {
   return (
-    <div id="image" className={`border-2 border-neutral-700 p-4 rounded-xl`}>
+    <>
       <div className={`relative w-56 h-56 shadow-lg shadow-neutral-600  `}>
         <Image
           src="/homeSource/chairSection2.jpg"
@@ -41,20 +41,20 @@ export const SecondSlider = () => {
       <div className={`flex flex-row justify-between mt-5 `}>
         <div className={``}>
           {" "}
-          <h2> $10000 </h2>{" "}
+          <h2> $39.999 </h2>{" "}
         </div>
         <div className={``}>
           {" "}
-          <h2> Silla Ergonomica </h2>{" "}
+          <h2> Silla de Pluma </h2>{" "}
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
 export const ThirdSlider = () => {
   return (
-    <div id="image" className={`border-2 border-neutral-700 p-4 rounded-xl`}>
+    <> 
       <div className={`relative w-56 h-56 shadow-lg shadow-neutral-600  `}>
         <Image
           src="/homeSource/chairSection3.jpg"
@@ -67,20 +67,20 @@ export const ThirdSlider = () => {
       <div className={`flex flex-row justify-between mt-5 `}>
         <div className={``}>
           {" "}
-          <h2> $10000 </h2>{" "}
+          <h2> $28.999 </h2>{" "}
         </div>
         <div className={``}>
           {" "}
-          <h2> Silla Ergonomica </h2>{" "}
+          <h2> Silla Terciopelo </h2>{" "}
         </div>
       </div>
-    </div>
+    </> 
   );
 };
 
 export const FourthSlider = () => {
   return (
-    <div id="image" className={`border-2 border-neutral-700 p-4 rounded-xl`}>
+    <>
       <div className={`relative w-56 h-56 shadow-lg shadow-neutral-600  `}>
         <Image
           src="/homeSource/chairSection4.jpg"
@@ -93,20 +93,20 @@ export const FourthSlider = () => {
       <div className={`flex flex-row justify-between mt-5 `}>
         <div className={``}>
           {" "}
-          <h2> $10000 </h2>{" "}
+          <h2> $12.000 </h2>{" "}
         </div>
         <div className={``}>
           {" "}
-          <h2> Silla Ergonomica </h2>{" "}
+          <h2> Silla Desplegable </h2>{" "}
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
 export const FiveSlider = () => {
     return (
-      <div id="image" className={`border-2 border-neutral-700 p-4 rounded-xl`}>
+      <>
         <div className={`relative w-56 h-56 shadow-lg shadow-neutral-600  `}>
           <Image
             src="/homeSource/chairSection5.jpg"
@@ -119,13 +119,13 @@ export const FiveSlider = () => {
         <div className={`flex flex-row justify-between mt-5 `}>
           <div className={``}>
             {" "}
-            <h2> $10000 </h2>{" "}
+            <h2> $6700 </h2>{" "}
           </div>
           <div className={``}>
             {" "}
-            <h2> Silla Ergonomica </h2>{" "}
+            <h2> Silla Plastica  </h2>{" "}
           </div>
         </div>
-      </div>
+      </>
     );
   };
